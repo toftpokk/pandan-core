d1efdd71da93149823a4d15814087f409e95ccc8
toftpokk <pakkapol.20100@gmail.com>
2023-07-20 16:37:57 +0700

    Feature: index.js

---
+++ src/lib/index.js
@@ -0,0 +1,5 @@
+// Reexport your entry components here
+import NavLink__SvelteComponent_ from "./NavLink.svelte";
+import Nav__SvelteComponent_ from "./Nav.svelte";
+export const NavLink = NavLink__SvelteComponent_
+export const Nav = Nav__SvelteComponent_
